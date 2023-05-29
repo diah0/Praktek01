fcaa07034af3463ea214f802f7029169b9871afc
diah <halimatus833@gmail.com>
2023-05-29 04:42:24 -0400

    Membuat Class Baru dan atribut serta behaviour

---
+++ src/main/java/com/mycompany/praktek01/Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktek01;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Mahasiswa {
+    String nama;
+    String NPM;
+    double IPK;
+    
+    void cetakInfo () {
+        System.out.println("Nama :" +nama);
+        System.out.println("NPM :" +NPM );
+        System.out.println("IPK :" +IPK );
+    }
+    
+    
+}
