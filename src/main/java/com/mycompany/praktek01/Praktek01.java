af916279b10b13ed82ed41e12b722a3da754a778
diah <halimatus833@gmail.com>
2023-05-29 04:19:16 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/praktek01/Praktek01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktek01;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Praktek01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
